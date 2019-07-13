9ddd188e60d6aec437f108dbb7ad673a1be5620d
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-07-13 15:09:33 +0100

    Fix bugs in routines

--- src/eulermaruyama.py
+++ src/eulermaruyama.py
@@ -16,10 +16,7 @@ class EulerMaruyama:
     def simulate(self, brownian: List[float] = None) -> List[float]:
         self.simulation = []
         current = self.initial
-        print(len(brownian))
-        print(self.steps)
         for i in range(self.steps-1):
-            print(i)
             self.simulation.append(current)
             normal = np.random.normal(0,1)
             if (brownian == None):
--- src/main.py
+++ src/main.py
@@ -2,20 +2,30 @@ from weiner import Weiner
 from blackscholes import BlackScholes
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
+from typing import List
+import numpy as np
 
-def run():
+def exact_soln(mu: float, sigma: float, delta: float, initial: float, brownian: List[float]):
+    exact = []
+    for i in range(len(brownian)-1):
+        exact.append(initial*np.exp((mu-sigma*sigma*0.5)*i*delta + sigma*brownian[i]))
+    return exact
+
+def run() -> None:
     mu = 0.75
     sigma = 0.3
     steps = 50
-    delta = 0.3
+    delta = 0.2
     initial = 307.65
     realisation = Weiner(steps, delta)
     realisation.simulate()
     eqn = BlackScholes(mu, sigma, steps, delta, initial)
     eqn.run(realisation.simulation)
-
+    exact = exact_soln(mu, sigma, initial, delta, realisation.simulation)
     time = [ n*delta for n in range(steps-1) ]
-    # smooth = interp1d(time, eqn.simulation)
+    smooth = interp1d(time, exact)
+    plt.plot(time, smooth(time))
+
     plt.scatter(time, eqn.simulation, s=0.5)
 
 
