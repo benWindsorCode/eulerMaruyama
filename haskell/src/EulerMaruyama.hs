8f53a89148b5dd4662c13662c47ab70e083823da
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-11-28 21:29:15 +0000

    Implement first pass at euler maruyama

--- haskell/src/EulerMaruyama.hs
+++ haskell/src/EulerMaruyama.hs
@@ -1,5 +1,42 @@
 module EulerMaruyama  
     (
-
+        solveBlackScholes
     ) where
-        
+  
+import RandomHelper
+
+type Steps = Int
+type Delta = Double
+type Brownian = [Double]
+
+-- Solves for SDE of the form dX_t = a(t, X_t)dt + b(t, X_t)dW_t
+-- So for Black Scholes we have aFunc = mu*X_t and bFunc = sigma*X_t
+data SDE = SDE { 
+    aFunc :: Int -> Double -> Double,
+    bFunc :: Int -> Double -> Double
+}
+
+-- todo: make this take a sigma and mu
+solveBlackScholes :: Steps -> Delta -> [Double] 
+solveBlackScholes n d = solve blackScholes n d
+        where
+            blackScholesFunc1 t x = 0.3*x
+            blackScholesFunc2 t x = 0.6*x
+            blackScholes = SDE { aFunc = blackScholesFunc1, bFunc = blackScholesFunc2 }
+
+solve :: SDE -> Steps -> Delta -> [Double]
+solve eqn n d = solveHelper eqn d 0 (normalSampleList n) []
+
+-- Initial condition: X_0 = 1
+-- X_{t+1} = X_t + a(t, X_t)*delta + b(t, X_t)*(i'th brownian) at the i'th step
+solveHelper :: SDE -> Delta -> Int -> Brownian -> [Double] -> [Double]
+solveHelper eqn d step brownian [] = solveHelper eqn d (step+1) (tail brownian) [1 + aVal*d + bVal*(head brownian)]
+        where
+            aVal = (aFunc eqn) step 1
+            bVal = (aFunc eqn) step 1
+solveHelper _ _ _ [] approx = approx
+solveHelper eqn d step brownian approx = solveHelper eqn d (step+1) (tail brownian) (approx ++ [currentX + aVal*d + bVal*(head brownian)])
+        where
+            currentX = last approx
+            aVal = (aFunc eqn) step currentX
+            bVal = (aFunc eqn) step currentX
--- haskell/src/RandomHelper.hs
+++ haskell/src/RandomHelper.hs
@@ -6,13 +6,14 @@ module RandomHelper
 import System.Random
 import Data.Random.Normal
 
-normalSampleList :: (Random a, Floating a) => Int -> [a]
+normalSampleList :: Int -> [Double]
 normalSampleList n = normalSampleHelper n (mkStdGen 0) []
 
-normalSampleHelper :: (Random a, Floating a) => Int -> StdGen -> [a] -> [a]
+normalSampleHelper :: Int -> StdGen -> [Double] -> [Double]
 normalSampleHelper 0 _ result = result
 normalSampleHelper n gen result = normalSampleHelper (n-1) newGen (result ++ [sample])
         where (sample, newGen) = normalSample gen
 
-normalSample :: (Random a, Floating a) => StdGen -> (a, StdGen)
-normalSample gen = normal gen
+normalSample :: StdGen -> (Double, StdGen)
+normalSample gen = (sample :: Double, newGen)
+    where (sample, newGen) = normal gen
