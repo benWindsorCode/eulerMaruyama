c107423cf14010e8a9dfda0376e5c529e9a931b8
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-11-28 20:26:59 +0000

    Reorganise haskell

--- haskell/src/EulerMaruyama.hs
+++ haskell/src/EulerMaruyama.hs
@@ -1,17 +1,5 @@
-module EulerMaruyama
-    ( 
+module EulerMaruyama  
+    (
+
     ) where
-
-import System.Random
-import Data.Random.Normal
-
-normalSampleList :: (Random a, Floating a) => Int -> [a]
-normalSampleList n = normalSampleHelper n (mkStdGen 0) []
-
-normalSampleHelper :: (Random a, Floating a) => Int -> StdGen -> [a] -> [a]
-normalSampleHelper 0 _ result = result
-normalSampleHelper n gen result = normalSampleHelper (n-1) newGen (result ++ [sample])
-        where (sample, newGen) = normalSample gen
-
-normalSample :: (Random a, Floating a) => StdGen -> (a, StdGen)
-normalSample gen = normal gen
+        
